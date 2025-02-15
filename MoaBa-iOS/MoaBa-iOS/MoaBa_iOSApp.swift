8201c6d114c0c153163a0e2b38da6d1ba83596d4
hsj._.06 <gtw030488@gmail.com>
2025-02-15 18:33:23 +0900

    init :: SwiftUI Project init

---
+++ MoaBa-iOS/MoaBa-iOS/MoaBa_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoaBa_iOSApp.swift
+//  MoaBa-iOS
+//
+//  Created by 홍승재 on 2/15/25.
+//
+
+import SwiftUI
+
+@main
+struct MoaBa_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
